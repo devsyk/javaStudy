9e63d3a286e1a816d28ba4f5af708933239b2cb7
devsyk <93830779+devsyk@users.noreply.github.com>
2022-08-17 21:59:16 +0900

    first commit

---
+++ src/ch2/Ex2_1.java
@@ -0,0 +1,18 @@
+package ch2;
+
+public class Ex2_1 {
+	public static void main(String[] args) {
+		//sysout ctrl + space 
+		//ctrl + alt + ↑↓ : 한줄 복사
+		// print() : 출력 후 줄바꿈 안함
+		// println() : 출력 후 줄바꿈함		
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		
+		System.out.println("5+3");
+		System.out.println("5-3");
+		System.out.println("5*3");
+		System.out.println("5/3");
+	}
+}
